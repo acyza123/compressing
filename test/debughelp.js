d83dc40976e8082bf06b0ad8a4270aa8d8ab4edd
acyza <101238421+acyza@users.noreply.github.com>
2022-12-23 22:00:52 +0800

    test

--- test/debughelp.js
+++ test/debughelp.js
@@ -35,7 +35,22 @@ function arrToStr(arr) {
   return result + ']';
 }
 
+function eqF(path1, path2) {
+  const dir1 = fs.readdirSync(path1);
+  const dir2 = fs.readdirSync(path2);
+  if (dir1.length !== dir2.length) return false;
+  for (let i = 0; i < dir1.length; i++) {
+    if (dir1[i] !== dir2[i]) return false;
+    if (fs.statSync(dir1[i])
+    .isDirectory() && !eqF(dir1[i], dir2[i])) {
+      throw dir1[i] + ':' + dir2[i];
+    }
+  }
+  return true;
+}
+
 module.exports.compareSync = function() {
+  eqF.apply(undefined, arguments);
   const result = _dircompare.compareSync.apply(undefined, arguments);
   result.diff = arrToStr(result.diffSet);
   if (result.equal === 4) return result;
