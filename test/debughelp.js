09e71af136e1da6923dacb0aa830623240325b24
acyza <101238421+acyza@users.noreply.github.com>
2022-12-23 19:53:40 +0800

    test

--- test/debughelp.js
+++ test/debughelp.js
@@ -1,13 +1,14 @@
 'use strict';
 const _dircompare = require('dir-compare');
 const fs = require('fs');
-module.export.compareSync = function() {
+module.exports.compareSync = function() {
   const result = _dircompare.compareSync.apply(undefined, arguments);
+  result.dir1 = fs.readdirSync(arguments[0]);
+  result.dir2 = fs.readdirSync(arguments[1]);
+  if (result.equal === 4) return result;
   console.log('-----------------------------------');
   console.log(result);
-  if (result.equal === 4) {
-    console.log('dir 1:%s \n %s', fs.readdirSync(arguments[0]), fs.readdirSync(arguments[1]));
-  }
+  console.log('dir 1:%s \n dir 2:%s', fs.readdirSync(arguments[0]), fs.readdirSync(arguments[1]));
   console.log('-----------------------------------');
   return result;
 };
