3f1a85640ea323f4c1d5cea549e2718bb7f664df
acyza <101238421+acyza@users.noreply.github.com>
2022-12-23 20:17:29 +0800

    test

--- test/debughelp.js
+++ test/debughelp.js
@@ -1,9 +1,25 @@
 'use strict';
 const _dircompare = require('dir-compare');
 const fs = require('fs');
+
+/**
+ * @param { Array<any> } arr array
+ * @return { string } string
+ */
+function arrToStr(arr) {
+  let result = '[';
+  for (const item in arr) {
+    if (Array.isArray(item)) {
+      result += arrToStr(item);
+    }
+    result += item.toString();
+  }
+  return result;
+}
+
 module.exports.compareSync = function() {
   const result = _dircompare.compareSync.apply(undefined, arguments);
-  result.diff = result.diffSet.toString();
+  result.diff = arrToStr(result.diffSet);
   if (result.equal === 4) return result;
   console.log('-----------------------------------');
   console.log(result);
